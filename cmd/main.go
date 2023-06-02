f8a1d467044d5370a46bf2206d60d0c82052c86f
swingkiddo <yoshibeatsu@gmail.com>
2023-06-02 16:44:32 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+	"github.com/swingkiddo/go-todo"
+	"github.com/swingkiddo/go-todo/pkg/handler"
+	"github.com/swingkiddo/go-todo/pkg/repository"
+	"github.com/swingkiddo/go-todo/pkg/service"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
---
+++ pkg/handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	servive "github.com/swingkiddo/go-todo/pkg/service"
+)
+
+type Handler struct {
+	services *servive.Service
+}
+
+func NewHandler(services *servive.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sing-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:item_id", h.getItemById)
+				items.PUT("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
+			}
+		}
+	}
+
+	return router
+}
